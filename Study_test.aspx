d63dd40ca7b3cb67a9e13f7e0617b1a88cc86d1a
SkyJin <Skyjin@outlook.com>
2017-07-06 17:07:45 +0800

    学习界面添加

---
+++ Study_test.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Study_test.aspx.cs" Inherits="Study_test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Image ID="Image1" runat="server" Height="117px" Width="164px"  ImageUrl="~/image/1.jpg" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:Button ID="Button1" runat="server" Height="36px" style="margin-left: 0px" Text="公告" Width="166px" OnClick="Button1_Click"  />
+        <br />
+        <asp:Button ID="Button2" runat="server" Height="36px" Text="课件学习" Width="166px" OnClick="Button2_Click" />
+        <br />
+        <asp:Button ID="Button3" runat="server" Height="36px" Text="测验与作业" Width="166px" OnClick="Button3_Click" />
+        <br />
+        <asp:Button ID="Button4" runat="server" Height="36px" Text="考试" Width="166px" />
+        <br />
+        <asp:Button ID="Button5" runat="server"  Height="36px" Text="讨论区" Width="166px" />
+        <br />
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Study_test.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Study_test : System.Web.UI.Page
+{
+    SqlConnection objConnection = new SqlConnection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+        using (objConnection)
+        {
+            string username = "forever";
+            string strSQL = "select Qid,Question from Questions";
+            SqlDataAdapter adapter = new SqlDataAdapter(strSQL, objConnection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            foreach (DataRowView drv in NewMethod(ds).DefaultView)
+            {
+                Response.Write(drv["Qid"] + "    " + drv["Question"]);
+
+            }
+        }
+        /*objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+        objConnection.Open();
+        String SqlStr = "Select  Qid,Question  From  Questions";
+        SqlCommand cmd = new SqlCommand(SqlStr, objConnection);
+        String st1 = (String)cmd.ExecuteScalar();
+        TextBox1.Text = st1;
+        objConnection.Close();*/
+    }
+
+    private static DataTable NewMethod(DataSet ds)
+    {
+        return ds.Tables[0];
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Study_notice.aspx");
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Mp4play.aspx");
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Study_test.aspx");
+    }
+}
