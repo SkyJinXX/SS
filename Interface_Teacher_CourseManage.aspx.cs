c561ca2c20e65aa2e93c755abf3b99c4507f4bc8
金石开 <Skyjin@outlook.com>
2017-08-19 15:38:04 +0800

    Sky_见鬼了

--- Interface_Teacher_CourseManage.aspx.cs
+++ Interface_Teacher_CourseManage.aspx.cs
@@ -4,11 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 public partial class Interface_Teacher_CourseManage : System.Web.UI.Page
 {
+    SqlConnection objConnection = new SqlConnection();
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (Session["username"] == null)
+        {
+            Response.Write("<script>alert('请先登录');window.location.href='default.aspx'</script>");
+
+        }
+        else if ((String)Session["identity"] != "T")
+        {
+            Response.Write("<script>alert('身份错误');window.location.href='default.aspx'</script>");
+        }
+        else if ((String)Session["Cid"] == null)
+        {
+            Response.Write("<script>alert('课程未定');window.location.href='Interface_Teacher_Manage'</script>");
+        }
+        else
+        {
+            Label1.Text = (String)Session["username"];
+        }
+        if (!IsPostBack)
+        {
+            objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+            string sql = "";
+            SqlCommand cmd = new SqlCommand(sql, objConnection);
+            cmd.CommandText = "select * from Course where Cid = '" + Session["Cid"] + "'";
+            objConnection.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            sdr.Read();          
+            TextBox1.Text = (string)sdr[0];
+            TextBox2.Text = (string)sdr[1];
+            TextBox3.Text = sdr[2].ToString();
+            TextBox4.Text = sdr[3].ToString();
+            DropDownList1.SelectedValue = (string)sdr[4];
+            TextBox6.Text = (string)sdr[5];
+            sdr.Close();
+            objConnection.Close();
+        }
 
     }
 
@@ -38,4 +76,9 @@ public partial class Interface_Teacher_CourseManage : System.Web.UI.Page
 
     }
 
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+
+    }
 }
