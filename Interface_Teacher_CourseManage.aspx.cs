cc112a22a2e12dc4f3aef1f12359becc4879c319
金石开 <Skyjin@outlook.com>
2017-08-19 16:06:52 +0800

    Sky_CourseManage_信息修改

--- Interface_Teacher_CourseManage.aspx.cs
+++ Interface_Teacher_CourseManage.aspx.cs
@@ -29,6 +29,7 @@ public partial class Interface_Teacher_CourseManage : System.Web.UI.Page
         {
             Label1.Text = (String)Session["username"];
         }
+        
         if (!IsPostBack)
         {
             objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
@@ -37,15 +38,22 @@ public partial class Interface_Teacher_CourseManage : System.Web.UI.Page
             cmd.CommandText = "select * from Course where Cid = '" + Session["Cid"] + "'";
             objConnection.Open();
             SqlDataReader sdr = cmd.ExecuteReader();
-            sdr.Read();          
-            TextBox1.Text = (string)sdr[0];
-            TextBox2.Text = (string)sdr[1];
-            TextBox3.Text = sdr[2].ToString();
-            TextBox4.Text = sdr[3].ToString();
-            DropDownList1.SelectedValue = (string)sdr[4];
-            TextBox6.Text = (string)sdr[5];
-            sdr.Close();
-            objConnection.Close();
+            try
+            {
+                sdr.Read();
+                TextBox1.Text = (string)sdr[0];
+                TextBox2.Text = (string)sdr[1];
+                TextBox3.Text = sdr[2].ToString();
+                TextBox4.Text = sdr[3].ToString();
+                DropDownList1.SelectedValue = (string)sdr[4];
+                TextBox6.Text = (string)sdr[5];
+                sdr.Close();
+                objConnection.Close();
+            }
+            catch (Exception)
+            {
+                Response.Write("<script>alert('出错啦')</script>");
+            }
         }
 
     }
@@ -79,6 +87,17 @@ public partial class Interface_Teacher_CourseManage : System.Web.UI.Page
 
     protected void Button7_Click(object sender, EventArgs e)
     {
-
+        string Cname = TextBox2.Text;
+        string Ccredit = TextBox3.Text;
+        string Cpersonnumber = TextBox4.Text;
+        string Ccategory = DropDownList1.SelectedValue;
+        string Cintroduction = TextBox6.Text;
+        objConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConStr"].ToString();
+        string sql = string.Format("update Course set Cname = '{0}', Ccredit = {1}, Cpersonnumber = {2}, Ccategory = '{3}', Cintroduction = '{4}' where Cid = '{5}'"
+            , Cname, Ccredit, Cpersonnumber, Ccategory, Cintroduction, (string)Session["Cid"]);
+        SqlCommand cmd = new SqlCommand(sql, objConnection);
+        objConnection.Open();
+        cmd.ExecuteNonQuery();
+        objConnection.Close();
     }
 }
